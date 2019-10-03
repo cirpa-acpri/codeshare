6b1d318dbef11c3e86dae202a3b8cf501e9743bb
Sopwith <frahay@gmail.com>
2019-10-03 08:28:29 -0400

    yea

---
+++ Python/Code/Excel Report Crawler.py
@@ -0,0 +1,63 @@
+# Fraser Hay - Conestoga College, Kitchener Ontario - fhay@conestogac.on.ca
+# -----------------------------------------------------------------------------
+# Short title: A script that isolated the comments from about 4,500 individual teacher evlaulation Excel reports.
+# -----------------------------------------------------------------------------
+# Longer description:
+# We found ourselves needing to provide a dataset of comments submitted by students in their
+# evaluations of teaching and learning, but we didn't have a source dataset - just the reports
+# themselves - all ~4,500 of them. Knowing Python could probably help, and armed with some ideas
+# from the internet (https://automatetheboringstuff.com/chapter12/), I set about turning a ridiculous
+# job into a computer science solution, as below. To do this, I had to cobble together a reference
+# "hit list" of all the report files (highlight all the files, CTRL+SHIFT+Right-click # on them >
+# Copy as path > paste in Excel). Then, since all my reports were exactly the same layout in Excel,
+# I knew I could just open them up, can copy out whatever I found in a particular area. 
+#
+# Looping through all 4,534 Excel report took about 15 minutes. But it seems to have worked
+# flawlessly. And doing this by hand would have been many days of insanity. Woo Python!
+# -----------------------------------------------------------------------------
+
+import time # Simple timer package - since this is going to be a big loop job, it'll be nice to keep track of how long it's been running.
+import openpyxl # This is the module that interacts with Excel files. https://openpyxl.readthedocs.io/en/stable/api/openpyxl.html
+Comments = [] # Define <Comments> as a list/array
+SAT_List = [] # Define <SAT_List> as a list/array
+counter = 0 # Housekeeping
+
+# Load the file "hit-list" into an array, just to make it easier to play with later. I mean we could probably loop through it line by line
+# a different way, but I just want to keep this simple.
+# Note: the random "r" denotes "real string". If I just put it in a string, Python tends to double \'s.
+ListFile = openpyxl.load_workbook(r"S:\Institutional_Research\SATs & Blue\Dashboard\SAT Files Listing.xlsx")
+sheet = ListFile.active
+# Note: range() is a built-in function of Python. It basically allows you to specify a Min/Max for a FOR loop.
+for SAT in range(1, sheet.max_row + 1):   #max_row is a (openpyxl) parameter of a sheet - specifying the last valid row on the sheet.
+    SAT_List.append(sheet.cell(row=SAT, column=1).value) # This loop just loops through all the rows, and encodes the values into the "hit-list" array.
+# Now <SAT_List[]> is an array including all entries from the Excel listing. 
+
+start = time.time() # Time checkpoint
+
+# Main sequence loop.
+for SAT in SAT_List: # For every SAT in our list, do the following...
+    counter += 1 # Housekeeping
+    print("Processing: " + str(counter) + " / " + str(len(SAT_List)) + " (" + str(int((counter / len(SAT_List) * 100))) + "%) - Time: " + str(round(time.time()-start, 1)) + "s") # Counter display, since this takes awhile.
+    workbook = openpyxl.load_workbook(SAT) # Open the file
+    sheet = workbook["Summary"] # Select the "Summary" Sheet
+    personInfo = sheet["A3"].value # Save A3 to a variable.
+    sheet = workbook["Comments"] # Move on to Comments. Select the sheet.
+    # Appending Comments: Loop through all the comments in the sheet
+    for row in range(3, sheet.max_row + 1): # Start on row 3, where comments begin; and for each row in the sheet...
+        if sheet["B" + str(row)].value: # This is a boolean check: is the B-cell of this row empty? This line is satisfied only if this is True.
+            course = sheet["B" + str(row)].value # <course> now equals the value of B<row> {Eg. if this is the first loop, we start at 3, so B3}.
+            comment = sheet["C" + str(row)].value # Similar to above, <comment> is populated with the comment
+            comment = comment.replace("_x000D_", "") # Get rid of some garbage that apparently is inherent in encoding carriage returns with openpyxl?
+            Comments.append([personInfo, course, comment]) # Append the identifiers, course ID and comment to a new line (list) in <Comments>
+        else:
+            break # If the row has no comment, we're done with this file. Next!
+
+print("\nFinal Encoding time: " + str(round(time.time()-start, 1)) + "s") # After all comments are read, display a final time for the run.
+            
+# Save data as a CSV
+import csv # Import the relevant module. https://docs.python.org/3/library/csv.html
+# Define the file, overwrite (if applicable), no characters needed to separate lines, utf-8 rich encoding, assign all this to the object <csv_file>
+with open(r"S:\Institutional_Research\SATs & Blue\Dashboard\Comments.csv", "w", newline="", encoding="utf-8-sig") as csv_file:
+    temp = csv.writer(csv_file) # <temp> will hold the <writer> function of the <csv> module we imported, and act on <csv_file>
+    for value in Comments: # For every row in <Comments>
+        temp.writerows([value]) # Write a new row to the file.
---
+++ Python/Code/File Sort.py
@@ -0,0 +1,51 @@
+# Fraser Hay - Conestoga College, Kitchener Ontario - fhay@conestogac.on.ca
+# -----------------------------------------------------------------------------
+# Short title: A script to sort files into folders based on a look-up table.
+# -----------------------------------------------------------------------------
+# Longer description:
+# Basically, we recieved a bunch of PDF reports from the province, which had program codes attached, 
+# but I needed to quickly upload them to a folder structure based on academic schools on our website.
+# So I made a look-up table of all the file names (highlight all the files, CTRL+SHIFT+Right-click 
+# on them > Copy as path > paste in Excel), isolated the program codes using formulas in Excel, then
+# looked up those codes in a program table I had to return their school. So at the end of that process,
+# I had a table of all the reports (paths), and all the schools. So I made folders corresponding to
+# those school names, and ran the script, and it sorted all the files into those folders based on the
+# look-up table. Fancy.
+
+# This is just an example of something I made the other day. Probably wasted a bit more time
+# on it than I should have, and probably re-invented the wheel in terms of there being easier ways
+# of doing this (like with JSON), but now I have something to show for it!
+# Related: https://automatetheboringstuff.com/chapter9/
+# -----------------------------------------------------------------------------
+
+
+import openpyxl # This is the module that interacts with Excel files. https://openpyxl.readthedocs.io/en/stable/api/openpyxl.html
+table = [] # Define my environment variables. "table" is going to be an array (list) of rows ("item")
+headers = [] # Array of the headers in the file
+item = {} # A dictionary that corresponds to a row of data, using the headers as the keys
+confused = {} # ... I don't know why, but I had to save things to this before it would work.
+
+# ------------------------
+
+file = openpyxl.load_workbook(r"C:\Users\fhay\Desktop\temp.xlsx") # Load the reference table.
+sheet = file.active # Set the active sheet to as an object.
+# Assumption is that first row has headers.
+for table_col in range(1, sheet.max_column + 1): # For every column...
+    headers.append(sheet.cell(row=1, column=table_col).value) # Write the headers to the array.
+
+for table_row in range(2, sheet.max_row + 1): # For every row in the file...
+    for header in headers: # For every header...
+        data = sheet.cell(row=table_row, column=headers.index(header) + 1).value
+        item[header] = data # Encode a header-data pairing. Eg: {"Gender": "Female"}
+    confused = item.copy() # I have no idea why, but I had to copy the item into another dictionary...
+    table.append(confused) # ... in order to save it to the "table" array...
+
+# Time to move around the files. Import the Shell Utilities module.
+import shutil # https://docs.python.org/3/library/shutil.html
+
+for x in table:  # For every item in the "table" array...
+    temp = (r"C:\Users\fhay\Desktop\New folder\"") # Playing with the string, I had trouble getting it 
+    temp = temp[:-1] # working the way I needed. This is basically me encoding an extra quote and then removing it.
+    temp = temp + x["School"] # Add "School" to the destination path of the file.
+    shutil.copy(x["Path"], temp) # Copy the source file to the destination path!
+    
