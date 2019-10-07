87444f75ff9d64e96934c6bb896ea3d88f99525b
Sopwith <frahay@gmail.com>
2019-10-07 09:42:20 -0400

    Update Excel Report Crawler.py

--- Python/Code/Excel Report Crawler.py
+++ Python/Code/Excel Report Crawler.py
@@ -26,9 +26,9 @@ counter = 0 # Housekeeping
 # a different way, but I just want to keep this simple.
 # Note: the random "r" denotes "real string". If I just put it in a string, Python tends to double \'s.
 ListFile = openpyxl.load_workbook(r"S:\Institutional_Research\SATs & Blue\Dashboard\SAT Files Listing.xlsx")
-sheet = ListFile.active
+sheet = ListFile.active # Make <sheet> the active sheet (upon loading the file).
 # Note: range() is a built-in function of Python. It basically allows you to specify a Min/Max for a FOR loop.
-for SAT in range(1, sheet.max_row + 1):   #max_row is a (openpyxl) parameter of a sheet - specifying the last valid row on the sheet.
+for SAT in range(1, sheet.max_row + 1):   # For each row in the file... Note: max_row is a (openpyxl) parameter of a sheet - specifying the last valid row on the sheet.
     SAT_List.append(sheet.cell(row=SAT, column=1).value) # This loop just loops through all the rows, and encodes the values into the "hit-list" array.
 # Now <SAT_List[]> is an array including all entries from the Excel listing. 
 
@@ -44,7 +44,7 @@ for SAT in SAT_List: # For every SAT in our list, do the following...
     sheet = workbook["Comments"] # Move on to Comments. Select the sheet.
     # Appending Comments: Loop through all the comments in the sheet
     for row in range(3, sheet.max_row + 1): # Start on row 3, where comments begin; and for each row in the sheet...
-        if sheet["B" + str(row)].value: # This is a boolean check: is the B-cell of this row empty? This line is satisfied only if this is True.
+        if sheet["B" + str(row)].value: # This is a boolean check: is the B-cell of this row empty? This line is satisfied only if this is True - meaning a value is present. If so, we proceed. If not, we break off the loop in this file.
             course = sheet["B" + str(row)].value # <course> now equals the value of B<row> {Eg. if this is the first loop, we start at 3, so B3}.
             comment = sheet["C" + str(row)].value # Similar to above, <comment> is populated with the comment
             comment = comment.replace("_x000D_", "") # Get rid of some garbage that apparently is inherent in encoding carriage returns with openpyxl?
@@ -59,5 +59,5 @@ import csv # Import the relevant module. https://docs.python.org/3/library/csv.h
 # Define the file, overwrite (if applicable), no characters needed to separate lines, utf-8 rich encoding, assign all this to the object <csv_file>
 with open(r"S:\Institutional_Research\SATs & Blue\Dashboard\Comments.csv", "w", newline="", encoding="utf-8-sig") as csv_file:
     temp = csv.writer(csv_file) # <temp> will hold the <writer> function of the <csv> module we imported, and act on <csv_file>
-    for value in Comments: # For every row in <Comments>
+    for value in Comments: # For every entity [ID/Course/Comment] in <Comments>
         temp.writerows([value]) # Write a new row to the file.
